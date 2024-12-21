e53d8c6d547e79804f2fec06362998469bdc10c3
tiagolimar <tiagolima.r2@gmail.com>
2024-12-21 08:39:25 -0300

    add function 'preencherEstado'

--- js/script.js
+++ js/script.js
@@ -5,13 +5,25 @@ async function obterDados(url){
 }
 
 async function preencherRegiao(){
-    const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes'
+    const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes';
     
     const dados = await obterDados(url);
     const regiao = document.getElementById('regiao');
+
+    for(const dado of dados){
+        regiao.innerHTML += `<option value="${dado.sigla}">${dado.nome}</option>`;
+    }
+}
+
+async function preencherEstado(){
+    const estado  = document.getElementById('estado');
+    const regiao = document.getElementById('regiao').value;
+    
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regiao}/estados`
+    const dados = await obterDados(url);
     
     for(const dado of dados){
-        regiao.innerHTML += `<option>${dado.nome}</option>`;
+        estado.innerHTML += `<option value="${dado.sigla}">${dado.nome}</option>`;
     }
 }
 
